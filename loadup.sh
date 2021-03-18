b12acbe2fc2dfcff4fa11afeb54bdf0dd9644cd4
Abdul <abdulhamid23@gmail.com>
2021-03-18 08:00:00 -0400

    Create loadup.sh

---
+++ loadup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install python3-pip -y
+
+sudo apt-get install mysql-server -y
+sudo systemctl start mysql
+sudo systemctl enable mysql
+
+# Acquiring and unzipping files
+sudo apt install unzip -y
+
+wget https://sp.mysqltutorial.org/wp-content/uploads/2018/03/mysqlsampledatabase.zip
+unzip mysqlsampledatabase.zip
+
+wget https://sp.mysqltutorial.org/wp-content/uploads/2018/03/python_mysql.zip
+unzip python_mysql.zip
