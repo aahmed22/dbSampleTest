8a25e0bad74206a390f3e8a024aefb6765907ff0
Abdul Ahmed <abdulhamid23@gmail.com>
2021-03-22 00:55:36 +0000

    adding additional files

---
+++ pandas/sample3.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+FT_cast = ['Natsu', 'Grey', 'Lucy', 32, 23]
+
+myvar = pd.Series(FT_cast)
+
+print(myvar)
+
+print(myvar[0])
+print(myvar[1])
+print(myvar[2])
+print(myvar[3])
